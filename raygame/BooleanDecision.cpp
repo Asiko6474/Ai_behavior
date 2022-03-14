3669ebbae922b9dbf4773c8291e404edbcf1ae75
Asiko6474 <Zacharycgrippo@gmail.com>
2022-03-14 16:59:29 -0500

    Added in decision classes

---
+++ raygame/BooleanDecision.cpp
@@ -0,0 +1,21 @@
+#include "BooleanDecision.h"
+
+void BooleanDecision::makeDecision(Agent* agent, float deltaTime)
+{
+	if (checkCondition(agent, deltaTime))
+	{
+		if (m_left)
+			m_left->makeDecision(agent, deltaTime);
+	}
+	else
+	{
+		if (m_right)
+			m_right->makeDecision(agent, deltaTime);
+	}
+}
+
+BooleanDecision::BooleanDecision(Decision* leftChild, Decision* rightChild)
+{
+	m_left = leftChild;
+	m_right = rightChild;
+}
---
+++ raygame/BooleanDecision.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Decision.h"
+
+/// <summary>
+/// A decision has two children attached
+/// </summary>
+class BooleanDecision :
+	public Decision
+{
+public:
+	/// <summary>
+	/// Checks the condition and transitions to a different decision based on the return value
+	/// </summary>
+	/// <param name="agent">The agent that this decision tree is attached to</param>
+	/// <param name="deltaTime">The amount of time between frames</param>
+	virtual void makeDecision(Agent* agent, float deltaTime) override;
+
+	/// <summary>
+	/// Checks if a certain condition has been met and returns the result
+	/// </summary>
+	/// <param name="agent"></param>
+	/// <param name="deltaTime"></param>
+	/// <returns></returns>
+	virtual bool checkCondition(Agent* agent, float deltaTime) { return false; }
+
+	/// <param name="leftChild">The left child of this node</param>
+	/// <param name="rightChild">The right child of this node</param>
+	BooleanDecision(Decision* leftChild = nullptr, Decision* rightChild = nullptr);
+
+private:
+	Decision* m_left;
+	Decision* m_right;
+};
+
